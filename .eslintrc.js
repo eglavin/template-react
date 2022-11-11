543eafd22545ec512a1276da69239f0f2bc8d172
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2022-11-11 20:49:10 +0000

    various clean ups

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 /**
- * https://eslint.org/docs/user-guide
+ * {@link https://eslint.org/docs/user-guide Eslint User Guide}
+ * @type {import('eslint').Linter.Config}
  */
-
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
@@ -21,20 +21,30 @@ module.exports = {
   },
   settings: {
     react: {
-      pragma: 'React',
-      version: 'detect',
+      pragma: "React",
+      version: "detect",
     },
   },
 
-  ignorePatterns: ['/node_modules', '*.test.*.snap'],
+  ignorePatterns: [
+    "/build",
+    "/dist",
+    "/node_modules",
+    "/coverage",
+    "/jest",
+    "*.test.*.snap",
+  ],
 
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier', '@typescript-eslint'],
+  plugins: ["react", "jsx-a11y", "prettier", "@typescript-eslint"],
   extends: [
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
 
   /**
@@ -45,96 +55,37 @@ module.exports = {
    * "error" or 2 - turn the rule on as an error (exit code is 1 when triggered)
    */
   rules: {
-    'no-console': 0,
-    'no-constant-condition': 1,
-
-    'no-unused-vars': 0,
-    '@typescript-eslint/no-unused-vars': 0,
-
-    'no-empty-function': 1,
-    '@typescript-eslint/no-empty-function': 1,
-
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-
-    'react-hooks/exhaustive-deps': 1,
-    'react-hooks/rules-of-hooks': 1,
-
-    'react/boolean-prop-naming': 1,
-    'react/display-name': 0,
-    'react/jsx-filename-extension': [
-      1,
-      {
-        extensions: ['.js', '.jsx', '.tsx'],
-      },
-    ],
-    'react/jsx-uses-vars': 2,
-    'react/prop-types': 0,
-
-    /* https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#eslint */
-    'react/jsx-uses-react': 0,
-    'react/react-in-jsx-scope': 0,
-
-    'jsx-a11y/no-noninteractive-element-interactions': [
-      2,
-      {
-        handlers: [
-          'onClick',
-          'onMouseDown',
-          'onMouseUp',
-          'onKeyPress',
-          'onKeyDown',
-          'onKeyUp',
-        ],
-      },
-    ],
-
-    'prettier/prettier': [
-      1,
-      {
-        endOfLine: 'auto',
-      },
-    ],
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-empty-function": 1,
+    "@typescript-eslint/no-unused-vars": 0,
+    "no-console": 0,
+    "no-constant-condition": 1,
+    "no-empty-function": 1,
+    "no-unused-vars": 0,
+    "prettier/prettier": [1, { endOfLine: "auto" }],
+    "react/display-name": 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
   },
 
   overrides: [
     {
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ["**/*.ts", "**/*.tsx"],
       rules: {
-        '@typescript-eslint/no-empty-interface': 0,
-        '@typescript-eslint/no-explicit-any': 1,
-
-        /**
-         * Require explicit return and argument types on exported functions' and
-         * classes' public class methods
-         */
-        '@typescript-eslint/explicit-module-boundary-types': 1,
-
-        /** Allowing non-null assertions using the ! postfix operator */
-        '@typescript-eslint/no-non-null-assertion': 0,
-
-        '@typescript-eslint/no-unused-vars': [
+        "@typescript-eslint/explicit-module-boundary-types": 1,
+        "@typescript-eslint/no-empty-interface": 0,
+        "@typescript-eslint/no-explicit-any": 1,
+        "@typescript-eslint/no-non-null-assertion": 0,
+        "@typescript-eslint/no-unused-vars": [
           1,
-          {
-            // Rest should not be reported as an issue as its an easy way to omit props.
-            ignoreRestSiblings: true,
-            // Allow property's that begin with an underscore character.
-            argsIgnorePattern: '^_',
-          },
+          { ignoreRestSiblings: true, argsIgnorePattern: "^_" },
         ],
       },
     },
     {
-      files: ['**/*.js', '**/*.jsx'],
+      files: ["**/*.js", "**/*.jsx"],
       rules: {
-        'no-unused-vars': [
-          1,
-          {
-            // Rest should not be reported as an issue as its an easy way to omit props.
-            ignoreRestSiblings: true,
-            // Allow property's that begin with an underscore character.
-            argsIgnorePattern: '^_',
-          },
-        ],
+        "no-unused-vars": [1, { ignoreRestSiblings: true, argsIgnorePattern: "^_" }],
       },
     },
   ],
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,27 +1,7 @@
 /**
- * https://prettier.io/docs/en/options.html
+ * {@link https://prettier.io/docs/en/options.html Prettier Options}
+ * @type {import('prettier').Options}
  */
-
 module.exports = {
-  arrowParens: 'always',
   printWidth: 90,
-  quoteProps: 'as-needed',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'es5',
-  overrides: [
-    {
-      files: ['*.test.{js,jsx,ts,tsx}'],
-      options: {
-        printWidth: 120,
-      },
-    },
-    {
-      files: ['*.html'],
-      options: {
-        tabWidth: 4,
-      },
-    },
-  ],
 };
